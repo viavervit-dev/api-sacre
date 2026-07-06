8c29e812e221931cf841d29ad5438ca194874f05
Carlos Andres Aguirre Ariza <120736166+The-Asintota@users.noreply.github.com>
2026-07-06 14:40:47 +0000

    add: router encargado de la funcionalidad de cierre de sesión

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ from src.config.parameters import settings
 from src.config.serialization import JSONResponse
 from src.modules.auth.routers.jwt.authenticate_admin import router as jwt_login_admin
 from src.modules.auth.routers.jwt.get_current_user import router as get_current_user
+from src.modules.auth.routers.jwt.logout import router as jwt_logout
 from src.modules.auth.routers.jwt.refresh import router as jwt_refresh
 from src.modules.customers.routers.create import router as create_customer
 from src.modules.inventory.routers.categories.create_category import router as get_category
@@ -98,6 +99,7 @@ v1_router.include_router(router=get_product)
 v1_router.include_router(router=create_prodcut)
 v1_router.include_router(router=update_product)
 v1_router.include_router(router=delete_product)
+v1_router.include_router(router=jwt_logout)
 v1_router.include_router(router=delete_category)
 app.include_router(router=v1_router)
 
--- src/modules/auth/routers/jwt/logout.py
+++ src/modules/auth/routers/jwt/logout.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.common.schema import (
@@ -8,17 +8,16 @@ from src.common.schema import (
     response_scheme_503,
 )
 from src.config.database import get_db_session
-from src.config.parameters import settings
 from src.modules.auth.dependencies import get_raw_tokens
 from src.modules.auth.repositories.user import UserRepository
-from src.modules.auth.services.jwt.refresh import RefreshTokenService
+from src.modules.auth.services.jwt.logout import LogoutService
 
 router = APIRouter(prefix="/authentication", tags=["Autenticación"])
 
 
 @router.get(
-    path="/jwt/refresh/",
-    response_description="**(NO_CONTENT)** Se crea un nuevo token de acceso.",
+    path="/jwt/logout/",
+    response_description="**(NO_CONTENT)** Se cierra la sesión del usuario.",
     status_code=status.HTTP_204_NO_CONTENT,
     responses={
         401: response_scheme_401(
@@ -26,31 +25,19 @@ router = APIRouter(prefix="/authentication", tags=["Autenticación"])
             refresh_jwt_missing=True,
             access_jwt_invalid=True,
             refresh_jwt_invalid=True,
-            access_jwt_not_expired=True,
+            access_jwt_expired=True,
             refresh_jwt_expired=True,
             auth_session_expired=True,
         ),
         503: response_scheme_503(db_unavailable=True),
     },
 )
-async def refresh_token(
+async def logout(
     tokens: Annotated[tuple[str, str], Depends(get_raw_tokens)],
     db: Annotated[AsyncSession, Depends(get_db_session)],
-    response: Response,
 ) -> None:
-    """Endpoint para refrescar el token de acceso utilizando un token de actualización válido."""
+    """Endpoint para cerrar la sesión del usuario."""
 
     access_token, refresh_token = tokens
-    service = RefreshTokenService(user_repo=UserRepository, db=db)
-    new_access_token = await service.refresh_token(
-        access_token=access_token,
-        refresh_token=refresh_token,
-    )
-    response.set_cookie(
-        key="access_token",
-        value=new_access_token,
-        httponly=True,
-        secure=not settings.debug,
-        samesite="lax",
-        max_age=settings.access_token_expire * 60,
-    )
+    service = LogoutService(user_repo=UserRepository, db=db)
+    await service.logout(access_token=access_token, refresh_token=refresh_token)
